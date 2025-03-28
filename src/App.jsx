081781d4e384d769dd5b11e69a546464a8a089bb
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-03-28 00:58:24 -0400

    create quiz ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [index, setIndex] = useState(Math.floor(Math.random()* quiz.length))
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id="q">
+      <p dangerouslySetInnerHTML={{__html:quiz[index].q}}></p>
+    </div>
+    <div id="pic">
+      <img src={quiz[index].pic} />
+    </div>
+    <div id="a">
+    <p dangerouslySetInnerHTML={{__html:quiz[index].a}}></p>
+    </div>
     </>
   )
 }
 
-export default App
